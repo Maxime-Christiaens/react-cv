3eab17fe756803c511267bab11c18f8bf43d84bd
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2019-02-11 15:08:54 +0100

    things done

--- react-cv-linux/src/Compenent/FirstPage.js
+++ react-cv-linux/src/Compenent/FirstPage.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Slide from './slides/slide';
+import Formation from './slides/formations';
 import Button from './button';
 
 //création de l'objet H2
@@ -12,7 +13,7 @@ export default class FirstPage extends Component {
         this.state = {
             FirstPageAnimation : "",
             Animation : "",
-            id : "nope"
+            id : ""
         };
     }
     //Animations de la page d'acceuil
@@ -79,7 +80,9 @@ export default class FirstPage extends Component {
                     <Button texte="Technologies" icone="far fa-arrow-alt-circle-down" onClick={ () => { this.FirstPageOpening(); this.up(); this.id(3) /*Cette methode me permet de récupérer un "id" me permettant par la suite d'activer le slide correspondant */ } }/>
                 </div>  
             </div>
-            <Slide ClassName={ this.state.id=="1"?slideClass:"" /* La récupération de l'id me permet de choisir lequelle des slides doit être activer */} onClick={ () => { this.FirstPageClosing(); this.down()} } h2="hello billy1" />
+            <Slide ClassName={ this.state.id=="1"?slideClass:"" /* La récupération de l'id me permet de choisir lequelle des slides doit être activer */} onClick={ () => { this.FirstPageClosing(); this.down()} } h2="Formations">
+                <Formation/>
+            </Slide>
             <Slide ClassName={ this.state.id=="2"?slideClass:"" } onClick={ () => { this.FirstPageClosing(); this.down() } } h2="hello billy2" />
             <Slide ClassName={ this.state.id=="3"?slideClass:"" } onClick={ () => { this.FirstPageClosing(); this.down() } } h2="hello billy3" />
             </Fragment>
--- react-cv-linux/src/Compenent/slides/formations.js
+++ react-cv-linux/src/Compenent/slides/formations.js
@@ -1,1 +1,10 @@
-import FirstPage from './Compenent/FirstPage';
+import React, { Component } from 'react';
+
+export default class Formation extends Component{
+
+    render() {
+        return (
+            <h3>Web Developer Junior</h3>
+        );
+      }
+}

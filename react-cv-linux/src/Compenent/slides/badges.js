a2227e3842f6fd81141b6a9e61ee59fcb233308c
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2019-02-12 16:47:47 +0100

    badge in progress

--- react-cv-linux/src/Compenent/slides/badges.js
+++ react-cv-linux/src/Compenent/slides/badges.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Badgr from './smallObject/badgr';
 
 export default class Badge extends Component {
 
     render() {
         return (
+            /*
             <div className="badges">
-               
+                <div>
+                    <iframe src="https://api.badgr.io/public/assertions/1nhvHBZNRFCnp6UjB7wD_g?embedVersion=1" title="Badge: HTML 5"></iframe>
+                    <iframe src="https://api.badgr.io/public/assertions/4F3y9yS1RHy3gAn59sgrog?embedVersion=1" title="Badge: CSS3 - level 2"></iframe>
+                </div>
+                <div>
+                    <iframe src="https://api.badgr.io/public/assertions/E-VU94QrQ3GWaqnDg1q2Og?embedVersion=1" title="Badge: Responsiveness skills"></iframe>
+                    <iframe src="https://api.badgr.io/public/assertions/DdT1JSu5S8-3hSNh2Z4-1g?embedVersion=1" title="Badge: CSS framework - level 2"></iframe>
+                </div>
+                <div>
+                <iframe src="https://api.badgr.io/public/assertions/LrHvBOAWQY2hZ96zFo-2jA?embedVersion=1" title="Badge: Git - level 1"></iframe>
+                <iframe src="https://api.badgr.io/public/assertions/MC-tG3ORSj-O_eW6hAsiTQ?embedVersion=1" title="Badge: Terminal user"></iframe>
+                </div>
             </div>
+            */
+            <Fragment>
+                <Badgr/>
+            </Fragment>
         );
     }
 }
---
+++ react-cv-linux/src/Compenent/slides/smallObject/badgr.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Badgr extends Component {
+
+    render() {
+        return (
+            <div className="badgr">
+                <a className="flex-column-center" href="https://badgr.io/public/assertions/1nhvHBZNRFCnp6UjB7wD_g">
+                    <i class="fab fa-html5"></i>
+                    <p>HTML5</p>
+                </a>
+            </div>
+        );
+    }
+}

08c2766b864ffab032e1481bfe70d9e53cdf7a3f
maxime-christiaens <maxime.ad.christiaens@gmail.com>
2019-02-13 12:27:22 +0100

    hover transition added

--- src/Compenent/slides/Technologies.js
+++ src/Compenent/slides/Technologies.js
@@ -8,18 +8,16 @@ export default class Technologie extends Component {
         return (
             <Fragment>
                 <div className="technologies">
-                    <div className="row">
                         <div>
-                            <Techno i="fab fa-html5" p="Html5" link="https://www.w3.org/TR/html52/"/>
+                            <Techno i="fab fa-html5" p="Html5" link="https://www.w3schools.com/html/default.asp"/>
                             <Techno i="fab fa-js" p="JavaScript" link="https://www.w3schools.com/js/"/>
                             <Techno i="fab fa-linux" p="Linux" link="https://fr.wikipedia.org/wiki/Linux"/> 
                         </div>
                         <div>
                             <Techno i="fab fa-css3-alt" p="CSS 3" link="https://www.w3schools.com/css/default.asp"/>
-                            <Techno i="fab fa-php" p="PHP" link="http://php.net/"/>
+                            <Techno i="fab fa-php" p="PHP" link="https://www.w3schools.com/php/default.asp"/>
                             <Techno i="fas fa-code-branch" p="Git" link="https://git-scm.com/"/>
                         </div>
-                    </div>
                 </div>
             </Fragment>
         );

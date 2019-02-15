f65278a41a972f0c9860efb88ad92baf6ae513c8
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2019-02-15 15:29:23 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cv-react/precache-manifest.8c38657ca528bcd27286671cfb49aba6.js"
+  "/react-cv/precache-manifest.8af978b1b01299733b1b0f7a8ef90e93.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cv-react/index.html", {
+workbox.routing.registerNavigationRoute("/react-cv/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
